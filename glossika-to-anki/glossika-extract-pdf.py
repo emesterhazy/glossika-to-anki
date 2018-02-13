dc508aece2cedf428fc16095368018b18e1bb946
Evan Mesterhazy <evan.mesterhazy@gmail.com>
2018-02-13 07:37:24 -0500

    Wrap code in main() function
    Wrap all of the code in main() and use the
    __name__ == '__main__' paradigm to execute each script.
    
    This allows for a future update to include an interactive option
    for running the program.

--- glossika-to-anki/glossika-extract-pdf.py
+++ glossika-to-anki/glossika-extract-pdf.py
@@ -8,104 +8,108 @@ import re
 import subprocess
 import sys
 
+def main():
+    # Dictionary of languages and sentence markers.
+    # If your language isn't listed, add it here
 
-# Dictionary of languages and sentence markers.
-# If your language isn't listed, add it here
+    languages = {
+        'ZS': ['EN', '简', 'PIN']  # Mandarin
+    }
 
-languages = {
-    'ZS': ['EN', '简', 'PIN']  # Mandarin
-}
+    src_dir = 'glossika_src_data/pdf'
+    out_dir = 'glossika_output'
 
-src_dir = 'glossika_src_data/pdf'
-out_dir = 'glossika_output'
+    if not os.path.exists(src_dir):
+        os.makedirs(src_dir)
+        sys.exit('\nCreated {}\n'
+                 'Copy pdf files into this folder and re-run'.format(src_dir))
 
-if not os.path.exists(src_dir):
-    os.makedirs(src_dir)
-    sys.exit('\nCreated {}\n'
-             'Copy pdf files into this folder and re-run'.format(src_dir))
+    files = glob.glob('glossika_src_data/pdf/GLOSSIKA-EN*-EBK.pdf')
+    print('\nProcessing {} files...\n'.format(len(files)))
 
-files = glob.glob('glossika_src_data/pdf/GLOSSIKA-EN*-EBK.pdf')
-print('\nProcessing {} files...\n'.format(len(files)))
+    for f in sorted(files):
+        f_name = f.split('/')[-1]
+        print('Processing  {}'.format(f_name))
 
-for f in sorted(files):
-    f_name = f.split('/')[-1]
-    print('Processing  {}'.format(f_name))
-
-    re_match = re.search('(GLOSSIKA-EN(.{2,4})-..-EBK)\.pdf', f)
-    if not re_match:
-        print('File name does not match\n'
-              'Skipping {}'.format(f_name))
-        continue
-
-    csv_file = '{}/{}.tsv'.format(out_dir, re_match.group(1))
-    text_pdf = '{}/{}.txt'.format(out_dir, re_match.group(1))
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    # Set language flags to search for
-    if re_match.group(2) in languages:
-        lang_flags = languages.get(re_match.group(2))
-    else:
-        lang_flags = ['EN', re_match.group(2)]
-
-    # Try to delete text version of pdf if it exists
-    if os.path.exists(text_pdf):
-        try:
-            os.remove(text_pdf)
-        except Exception as e:
-            pass
-    if os.path.exists(text_pdf):
-        print('File locked... using existing text version of pdf')
-    else:
-        # Convert with pdftotext
-        st = subprocess.run(
-            args=['pdftotext', '-layout', '-enc', 'UTF-8', f, text_pdf],
-            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        if st.returncode != 0:
-            sys.exit('PDF conversion failed. Check file for corruption.\n'
-                     'Aborting at {}'.format(f_name))
-
-    pdf = open(text_pdf, encoding='utf-8')
-    lang_ix = 0  # Track which type of phrase is next
-    indent_level = 0  # Track indentation for muiti-line sentences
-    current_phrase = []  # Set of phrases
-    phrases = []
-    for line in pdf:
-        line = line.rstrip('\n')
-
-        if indent_level > 0:
-            # Match continuing sentence based on indentation level
-            r = re.match('(\s{{{}}})(\S.*)'.format(indent_level), line)
-            if r:
-                current_sent += ' ' + r.group(2)
-                continue
-            else:
-                indent_level = 0
-                lang_ix += 1
-                current_phrase.append(current_sent)
-                if lang_ix >= len(lang_flags):
-                    lang_ix = 0
-                    phrases.append(current_phrase)
-                    current_phrase = []
-
-        # Match sentence by finding language flag
-        r = re.match('(\s+{}\s+)(\S.*)'.format(lang_flags[lang_ix]), line)
-        if not r:
+        re_match = re.search('(GLOSSIKA-EN(.{2,4})-..-EBK)\.pdf', f)
+        if not re_match:
+            print('File name does not match\n'
+                  'Skipping {}'.format(f_name))
             continue
-        current_sent = r.group(2)
-        indent_level = len(r.group(1))
 
-    if len(phrases) != 1000:
-        print('Something went wrong...'
-              'found {} sentences instead of 1,000'.format(len(phrases)))
-        continue
+        csv_file = '{}/{}.tsv'.format(out_dir, re_match.group(1))
+        text_pdf = '{}/{}.txt'.format(out_dir, re_match.group(1))
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
 
-    with open(csv_file, 'w', newline='') as f:
-        writer = csv.writer(f, dialect='excel-tab')
-        for phrase in phrases:
-            writer.writerow(phrase)
-    pdf.close()
-    os.remove(text_pdf)
-    print('Finished    {}\n'.format(f_name))
+        # Set language flags to search for
+        if re_match.group(2) in languages:
+            lang_flags = languages.get(re_match.group(2))
+        else:
+            lang_flags = ['EN', re_match.group(2)]
 
-print('Complete!')
+        # Try to delete text version of pdf if it exists
+        if os.path.exists(text_pdf):
+            try:
+                os.remove(text_pdf)
+            except Exception as e:
+                pass
+        if os.path.exists(text_pdf):
+            print('File locked... using existing text version of pdf')
+        else:
+            # Convert with pdftotext
+            st = subprocess.run(
+                args=['pdftotext', '-layout', '-enc', 'UTF-8', f, text_pdf],
+                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if st.returncode != 0:
+                sys.exit('PDF conversion failed. Check file for corruption.\n'
+                         'Aborting at {}'.format(f_name))
+
+        pdf = open(text_pdf, encoding='utf-8')
+        lang_ix = 0  # Track which type of phrase is next
+        indent_level = 0  # Track indentation for muiti-line sentences
+        current_phrase = []  # Set of phrases
+        phrases = []
+        for line in pdf:
+            line = line.rstrip('\n')
+
+            if indent_level > 0:
+                # Match continuing sentence based on indentation level
+                r = re.match('(\s{{{}}})(\S.*)'.format(indent_level), line)
+                if r:
+                    current_sent += ' ' + r.group(2)
+                    continue
+                else:
+                    indent_level = 0
+                    lang_ix += 1
+                    current_phrase.append(current_sent)
+                    if lang_ix >= len(lang_flags):
+                        lang_ix = 0
+                        phrases.append(current_phrase)
+                        current_phrase = []
+
+            # Match sentence by finding language flag
+            r = re.match('(\s+{}\s+)(\S.*)'.format(lang_flags[lang_ix]), line)
+            if not r:
+                continue
+            current_sent = r.group(2)
+            indent_level = len(r.group(1))
+
+        if len(phrases) != 1000:
+            print('Something went wrong...'
+                  'found {} sentences instead of 1,000'.format(len(phrases)))
+            continue
+
+        with open(csv_file, 'w', newline='') as f:
+            writer = csv.writer(f, dialect='excel-tab')
+            for phrase in phrases:
+                writer.writerow(phrase)
+        pdf.close()
+        os.remove(text_pdf)
+        print('Finished    {}\n'.format(f_name))
+
+    print('Complete!')
+
+
+if __name__ == '__main__':
+    main()
